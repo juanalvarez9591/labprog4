506e04c63df1ad737252e98d82b1aab2953db81c
John <john.long.pino@fing.edu.uy>
2024-06-06 19:25:17 -0300

    Inicio de Comentarios
    empeze a hacer comentarios, hago push para que puedan ir viendo

--- include/Comentario.h
+++ include/Comentario.h
@@ -2,14 +2,30 @@
 #define COMENTARIO_H
 
 #include <string>
+using namespace std;
 
 class Comentario {
 public:
-    Comentario(std::string autor, std::string texto);
+
+    //falta agregar lo de la fecha(En su DT) y crea un comentario que no es apuntado ni apunta a nada, el control se va a tener que hacer una funcion de agragar cuando se haga
+    Comentario(string autor, string texto);
+
+    //Libera el DTfecha asociado
+    void Liberar();
+
+    Comentario* getSig();
+    Comentario* getResp();
+    DTFecha* getFecha();
+
+    //Devuelve el puntero a la primera respuesta, y puedes navegar al resto luego
+    Comentario* borrarRespuestas();
 
 private:
-    std::string autor;
-    std::string texto;
+    DTFecha date;
+    string autor;
+    string texto;
+    Comentario* Sigcomenario;
+    Comentario* Respuesta
 };
 
 #endif
---
+++ src/Comentario.cpp
@@ -0,0 +1,30 @@
+#include "Comentario.h"
+#include "DTComentario.h"
+#include "DTFecha.h"
+
+Comentario Comentario(std::string autor, std::string texto){
+	this->autor = autor;
+	this->texto = texto;
+	this->date = getDTFechaNow();
+	this->Sigcomenario = NULL;
+	this->Respuesta = NULL;
+}
+
+//Libera la el DTfecha asociado, hay que hacer delete aparte
+void Liberar(){
+	LiberarFecha(this->date);
+}
+
+//Devuelve el puntero a la primera respuesta
+Comentario* borrarRespuestas(){
+	return this->Respuesta;
+}
+
+string obtenerTexto(){
+
+	return this->texto;
+}
+
+DTComentario getDTComentario(){
+	return DTComentario(this->autor , this->texto);
+}
